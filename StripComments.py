ce15d87126691230ff74b19281e0b564d589dc2a
Alexandr Volkovich <volkoviysash@gmail.com>
2022-09-22 14:09:10 +0300

    Add function

--- StripComments.py
+++ StripComments.py
@@ -1,18 +1,21 @@
-strng = "apples, pears \ngrapes\nbananas"
+strng = "apples, pears #and bananas \ngrapes\nbananas"
 markers = ['#', '!']
 
 #making list of lines
-strngList = list(strng.split("\n"))
-strngoutput = ""
-strngarray = []
-
-for line in strngList:
+def strip_comments():
+    strngList = list(strng.split("\n"))
     strngoutput = ""
-    for c in line:
-        if c in markers:
-            break
-        else:
-            strngoutput += c
-    strngarray.append(strngoutput.rstrip())
+    strngarray = []
 
-print('\n'.join(strngarray))
+    for line in strngList:
+        strngoutput = ""
+        for c in line:
+            if c in markers:
+                break
+            else:
+                strngoutput += c
+        strngarray.append(strngoutput.rstrip())
+    print('\n'.join(strngarray))
+
+if __name__ == "__main__":
+    strip_comments()
