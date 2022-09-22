78e8b0d167f19e839dd38edcef3f8d0b032d9c0d
Alexandr Volkovich <volkoviysash@gmail.com>
2022-09-22 17:50:06 +0300

    Update syntax

--- isalpha.py
+++ isalpha.py
@@ -1,5 +1,9 @@
-password = ""
-for c in password:
-    if c.isalpha() or c.isdigit():
-        print(True)
-print(False)
+def check():
+    password = ""
+    for c in password:
+        if c.isalpha() or c.isdigit():
+            print(True)
+    print(False)
+    
+if __name__ == "__mian__":
+    main()
