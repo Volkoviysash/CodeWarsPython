3b78a054422d9732eee8caf179892b8812cf3d63
Alexandr Volkovich <volkoviysash@gmail.com>
2022-09-22 17:50:18 +0300

    Delete isalpha.py

--- isalpha.py
+++
@@ -1,9 +0,0 @@
-def check():
-    password = ""
-    for c in password:
-        if c.isalpha() or c.isdigit():
-            print(True)
-    print(False)
-    
-if __name__ == "__mian__":
-    main()
