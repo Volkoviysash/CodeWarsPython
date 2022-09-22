a615750b5e285f1d0d34e56c53f8a09ebf3e0e75
Alexandr Volkovich <volkoviysash@gmail.com>
2022-09-22 14:00:56 +0300

    Update functions

--- SimplePigLatin.py
+++ SimplePigLatin.py
@@ -1,22 +1,28 @@
+#Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
 text = "O Pig latin, is cool !"
 
-textList = list(text.split(" "))
-outp = ""
-
-# [Pig] [latin,] [is] [cool]"
-for word in textList:    
-    if (not word[len(word)-1].isalpha()) and len(word) == 1:
-        outp += word
-    elif len(word) == 1:
-        outp += word[0] + "ay"
-    else:
+def main():
+    pig_it(text)
         
-        while ind<=len(word)-2:
-            outp+=word[ind]
-            ind+=1
-        if word[len(word)-1].isalpha():
-            outp += word[len(word)-1] + word[0] + "ay "
-        else:
-            outp += word[0] + "ay" + word[len(word)-1] + " "
 
-print(outp.rstrip())
+def pig_it(text):
+    textList = list(text.split(" "))
+    outp = ""    
+    for word in textList:    
+        if (not word[len(word)-1].isalpha()) and len(word) == 1:
+            outp += word
+        elif len(word) == 1:
+            outp += word[0] + "ay "
+        else:
+            ind = 1
+            while ind<=len(word)-2:
+                outp+=word[ind]
+                ind+=1
+            if word[len(word)-1].isalpha():
+                outp += word[len(word)-1] + word[0] + "ay "
+            else:
+                outp += word[0] + "ay" + word[len(word)-1] + " "
+    return(outp.rstrip())
+
+if __name__ == "__main__":
+    main()
