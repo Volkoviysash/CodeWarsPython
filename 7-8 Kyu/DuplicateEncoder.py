b6882da7b875cd683e6188486c73837da023e901
Alexandr Volkovich <volkoviysash@gmail.com>
2022-09-22 13:47:35 +0300

    Add file

---
+++ 7-8 Kyu/DuplicateEncoder.py
@@ -0,0 +1,21 @@
+def main():
+    word = input("Input: ")
+    print(encoder(word))
+    
+    
+def encoder(word):
+    outp = ""
+    for c in word:
+        if word.count(c.lower()) > 1:
+            outp += ')'
+        else:
+            outp += '('
+    return outp
+
+
+if __name__ == "__main__":
+    main()
+
+#((()(((((((((()((((())((((((((()((
+#((()(((((((((()((((())((((((((()((
+#))()))))()())))))()())(()()))())))
