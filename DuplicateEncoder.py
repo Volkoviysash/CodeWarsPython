834036f65330f7d8c181fc4a0e49c847dab4299f
Alexandr Volkovich <volkoviysash@gmail.com>
2022-09-22 13:46:22 +0300

    Add main function and encoder function

--- DuplicateEncoder.py
+++ DuplicateEncoder.py
@@ -1,11 +1,20 @@
-world = input("Input: ")
-outp = ""
-for c in word:
-    if word.count(c.lower()) > 1:
-        outp += ')'
-    else:
-        outp += '('
-print(outp)
+def main():
+    word = input("Input: ")
+    print(encoder(word))
+    
+    
+def encoder(word):
+    outp = ""
+    for c in word:
+        if word.count(c.lower()) > 1:
+            outp += ')'
+        else:
+            outp += '('
+    return outp
+
+
+if __name__ == "__main__":
+    main()
 
 #((()(((((((((()((((())((((((((()((
 #((()(((((((((()((((())((((((((()((
