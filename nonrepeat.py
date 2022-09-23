c6b8d22c5c65b6569fbcad184ede65b3f88412f5
Alexandr Volkovich <volkoviysash@gmail.com>
2022-09-23 20:15:01 +0300

    Update nonrepeat.py

--- nonrepeat.py
+++ nonrepeat.py
@@ -1,7 +1,12 @@
-string = "abcdeaB"
-string = string.lower()
-outp = ""
-for c in string:
-    if string.count(c) > 1 and (not c in outp):
-        outp += c        
-print(len(outp))
+def main():
+    text = input("Input text: ")
+    print(f"Quantity of unique elements ={unique(text)}")
+
+
+def unique(string):
+    string = string.lower()
+    outp = 0
+    for c in string:
+        if string.count(c) > 1 and (not c in outp):
+            outp += 1      
+    return outpt
