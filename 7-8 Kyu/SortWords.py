a036775f00b8ef5b0b9ac9c833b65b7404c19aca
Alexandr Volkovich <volkoviysash@gmail.com>
2022-10-26 23:37:04 +0300

    Create sortwords.py

---
+++ 7-8 Kyu/SortWords.py
@@ -0,0 +1,16 @@
+from curses.ascii import isdigit
+
+sentence = "is2 Thi1s T4est 3a"
+wordsarr = sentence.split(" ")
+output = ""
+
+dict = {}
+for word in wordsarr:
+    for c in word:
+        if c.isdigit():
+            dict[word] = int(c)
+
+for word in sorted(dict.items(), key=lambda x: x[1]):
+    output += word[0] + ' '
+
+print(output.rstrip())
